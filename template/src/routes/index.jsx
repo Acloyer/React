c401cc16a65eabc955fa00fd3780b95fc7aeb6ed
Acloyer <93879175+Acloyer@users.noreply.github.com>
2023-11-22 20:02:21 +0400

    Deleted index jsx page source
    Удалил текст из index чтобы было чисто

--- template/src/routes/index.jsx
+++ template/src/routes/index.jsx
@@ -1,7 +1,7 @@
 function Index() {
     return (
         <p id="zero-state">
-            TaskMaster Ultimate Pro Max 666000
+            {/* TaskMaster Ultimate Pro Max 666000 */}
             {/* Check out{" "} */}
             {/* <a href="https://reactrouter.com">
                 the docs at reactrouter.com
